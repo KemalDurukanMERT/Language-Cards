77f3b0939af4c1fd03b8ad3ad0cd743a98095158
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-10-18 04:04:08 +0200

    version 3

--- solution/src/components/Language.jsx
+++ solution/src/components/Language.jsx
@@ -1,5 +1,30 @@
-const Language = ({}) => {
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import { useState } from "react";
 
+const Language = ({name, img, options}) => {
+    const [showImage, setShowImage] = useState(true);
+    return (
+        <Container
+            type = "button"
+            onClick={() => setShowImage(!showImage)}
+        >
+            {showImage && (
+                <Container>
+                    <Image src={img} width="70%"></Image>
+                    <h3>{name}</h3>
+                </Container>
+            )}
+
+            {!showImage && (
+                <ol>
+                    {options.map((item) => {
+                        return <li>{item}</li>
+                    })}
+                </ol>
+            )}
+        </Container>
+    )
 }
 
 export default Language;
